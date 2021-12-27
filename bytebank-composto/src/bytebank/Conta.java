791235928518f988477591105908e225aa4006ab
uJoao22 <joaopaniciobotelho@hotmail.com>
2021-12-27 12:23:56 -0300

    Composição de Objetos

---
+++ bytebank-composto/src/bytebank/Conta.java
@@ -0,0 +1,31 @@
+package bytebank;
+
+public class Conta {
+	double saldo;
+	int agencia;
+	int numero;
+	
+	//Criando a referencia para o objeto Cliente
+	Cliente titular;
+	
+	public void deposita(double valor){
+		this.saldo += valor;
+	}
+	
+	public boolean saca(double valor) {
+		if(valor <= this.saldo) {
+			this.saldo -= valor; 
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean transfere(double valor, Conta destino) {
+		boolean res = saca(valor);
+		if(res) {
+			destino.deposita(valor);
+			return true;
+		}
+		return false;
+	}
+}
---
+++ bytebank-composto/src/bytebank/TestaBanco.java
@@ -0,0 +1,18 @@
+package bytebank;
+
+public class TestaBanco {
+	public static void main(String[] args) {
+		Cliente paulo = new Cliente();
+		paulo.nome = "Paulo Silveira";
+		paulo.cpf = "222.222.222-22";
+		paulo.profissao = "Programador";
+		
+		Conta contaDoPaulo =  new Conta();
+		contaDoPaulo.deposita(100);
+		
+		//Definindo ao atributo titular a referencia do objeto Cliente 
+		contaDoPaulo.titular = paulo;
+		
+		System.out.println(contaDoPaulo.titular.nome);
+	}
+}
